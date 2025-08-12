1a7bddcf30a356701361a2d0073ff9b9818f3c55
Jannis R <mail@jannisr.de>
2025-08-12 14:01:35 +0200

    CI: adapt to latest GitHub Releases API 💚

--- ci/install-qsv.sh
+++ ci/install-qsv.sh
@@ -9,10 +9,18 @@ sudo apt install -y \
 
 ua='bbnavi/gtfs-flex CI'
 qsv_release='6.0.1'
-releases_url="https://api.github.com/repos/dathere/qsv/releases/$qsv_release"
+# `…/releases/tag/$tag` currently fails despite being documented:
+# https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#get-a-release-by-tag-name
+# see also https://github.com/orgs/community/discussions/169572
+# release_url="https://api.github.com/repos/dathere/qsv/releases/tag/$qsv_release"
+# assets_url="$(
+# 	curl "$release_url" -H 'Accept: application/json' -H "User-Agent: $ua" -L -fsS \
+# 	| jq -r '.assets_url'
+# )"
+releases_url='https://api.github.com/repos/dathere/qsv/releases'
 assets_url="$(
 	curl "$releases_url" -H 'Accept: application/json' -H "User-Agent: $ua" -L -fsS \
-	| jq -r '.assets_url'
+	| jq -r ".[] | select(.tag_name | test(\"$qsv_release\")) | .assets_url"
 )"
 qsv_x64_url="$(
 	curl "$assets_url" -H 'Accept: application/json' -H "User-Agent: $ua" -L -fsS \
